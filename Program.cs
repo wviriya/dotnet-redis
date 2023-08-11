ab99d7a902a2f7b76a51963af38fef3fb9fd4f14
Victor Viriya-ampanond <victor.ampanond@plexure.com>
2023-08-11 15:05:54 +1200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoTeamStats
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
+
+// dotnet user-secrets set CacheConnection "20.194.249.177:6379,abortConnect=false,ssl=false,allowAdmin=true"
